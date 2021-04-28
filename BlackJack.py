91c3d1c5bed4f042a21e55c73f5b15bc621bf46c
apple <josnag0809@highgateschool.org.uk>
2021-04-28 13:17:27 +0100

    testing

--- BlackJack.py
+++ BlackJack.py
@@ -3,31 +3,31 @@ import random
 my_suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 my_ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 my_values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
-          'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+             'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
+cards = (my_ranks, my_suits)
 
 
-class Cards:
+class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
     def __str__(self):
-        print(f'{self.value} of {self.rank}')
+        return self.suit + self.rank
 
 
 class Deck:
-    def __init__(self, suits, ranks):
-        self.deck = []
-        for suit in suits:
-            for rank in ranks:
-                #THIS IS WHERE YOU CREATE CARDS my_cards = Cards(my_suits, my_ranks)
-                self.deck.append(Cards(suit, rank))
+    def __init__(self):
+        self.decklist = []
 
-    def shuffle(self):
-        random.shuffle(self.deck)
-        print(self.deck)
+    def addcard(self, card):
+        self.decklist.append(card)
 
 
+my_deck = Deck()
 
-deck = Deck(my_suits, my_ranks)
-deck.shuffle()
+for s in my_suits:
+    for r in my_ranks:
+        a = Card(s, r)
+        my_deck.addcard(a)
